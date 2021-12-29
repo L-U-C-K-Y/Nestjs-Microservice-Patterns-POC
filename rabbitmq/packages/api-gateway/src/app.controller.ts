7da8fcf436f7d456c7cd3c1bbed3d59c99c5b935
Lucky Bassi <lucky@jobdone.app>
2021-12-29 17:01:57 +0100

    feat(rabbitmq): add console.time

--- rabbitmq/packages/api-gateway/src/app.controller.ts
+++ rabbitmq/packages/api-gateway/src/app.controller.ts
@@ -12,38 +12,52 @@ export class AppController {
 
   @Get('/first')
   testFirstService(): Observable<string> {
+    const now = new Date()
+    const time: string = now.toISOString()
+    console.time(time)
     const result =  this.firstClient.send<string>(
       { cmd: 'first_service' },
       'Message from',
     );
     console.log(result)
+    console.timeEnd(time)
     return result
   }
 
   @Get('/second')
   testSecondService(): Observable<string> {
+    const now = new Date()
+    const time: string = now.toISOString()
     const result = this.secondClient.send<string>(
       { cmd: 'second_service' },
       'Message from',
     );
     console.log(result)
+    console.timeEnd(time)
     return result
   }
 
   @Get('/third')
   testThirdService(): Observable<string> {
+    const now = new Date()
+    const time: string = now.toISOString()
     const result = this.thirdClient.send<string>(
       { cmd: 'third_service' },
       'Message from',
     );
     console.log(result)
+    console.timeEnd(time)
     return result
   }
 
   @Get('/undefined')
   testWhatService(): Observable<string> {
+    const now = new Date()
+    const time: string = now.toISOString()
+    console.time(time)
     const result = this.firstClient.send<string>({ cmd: 'undefined' }, 'Message from');
     console.log(result)
+    console.timeEnd(time)
     return result
   }
 
